2690ebc80ab66b7e628ea31fb7aeb657b0c4cc38
Aidan Simmons <aidanwsimmons621@gmail.com>
2023-07-18 17:26:25 -0700

    reinitializing repo

---
+++ stringArrayToNums.js
@@ -0,0 +1,12 @@
+// Create the function that takes as a parameter 
+// a sequence of numbers represented as strings and 
+// outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+
+function toNumberArray(stringarray){
+    return stringarray.map(el => +el)
+  }
