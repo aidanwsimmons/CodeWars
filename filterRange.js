c15bd6fe1fd17c9e8a5aea39559b19d291de561c
Aidan Simmons <aidanwsimmons621@gmail.com>
2023-10-04 12:24:23 -0700

    filter array problem

---
+++ filterRange.js
@@ -0,0 +1,25 @@
+function filterRange(arr, a, b){
+    return arr.filter(el => (el >= a && el <= b))
+  }
+
+//   describe("filterRange", function() {
+
+//     it("returns the filtered values", function() {
+  
+//       let arr = [5, 3, 8, 1];
+  
+//       let filtered = filterRange(arr, 1, 4); 
+  
+//       assert.deepEqual(filtered, [3, 1]);
+//     });
+  
+//     it("doesn't change the array", function() {
+  
+//       let arr = [5, 3, 8, 1];
+  
+//       let filtered = filterRange(arr, 1, 4); 
+  
+//       assert.deepEqual(arr, [5,3,8,1]);
+//     });
+  
+//   });
