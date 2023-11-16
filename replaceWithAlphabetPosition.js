ef925caeb5341b31cb72fa6ff2e51432d54dc498
Aidan Simmons <aidanwsimmons621@gmail.com>
2023-11-15 17:05:28 -0800

    6kyu string/array  solution

---
+++ replaceWithAlphabetPosition.js
@@ -0,0 +1,16 @@
+function alphabetPosition(text) {
+    let alphabet = ' abcdefghijklmnopqrstuvwxyz'.split('')
+    text = text.toLowerCase().split('')
+    let positions = text.map( (letter) => alphabet.indexOf(letter)).filter((position) => position > 0)
+    return positions.join(' ')
+  }
+  
+  // In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+  
+  // If anything in the text isn't a letter, ignore it and don't return it.
+  
+  // "a" = 1, "b" = 2, etc.
+  
+  // Example
+  // alphabetPosition("The sunset sets at twelve o' clock.")
+  // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
